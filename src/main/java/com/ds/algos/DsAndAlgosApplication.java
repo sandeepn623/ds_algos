4fc51f9935581edc8d07f14c8d5bac914c4fcc45
Sandeep Nanjundaswamy <sandeepn623@gmail.com>
2020-09-22 20:30:48 +0200

    initial commit

---
+++ src/main/java/com/ds/algos/DsAndAlgosApplication.java
@@ -0,0 +1,13 @@
+package com.ds.algos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAndAlgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAndAlgosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/algos/DsAndAlgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.algos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAndAlgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
